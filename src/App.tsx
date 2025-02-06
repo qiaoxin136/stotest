69a2d7a5b942e0f322b9ec7e4f32cc35b00dbd60
Xin Qiao <qiaoxin136@gmail.com>
2025-02-06 11:29:54 -0500

    start 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+//import { useEffect, useState } from "react";
+import { useAuthenticator } from '@aws-amplify/ui-react';
+//import type { Schema } from "../amplify/data/resource";
+//import { generateClient } from "aws-amplify/data";
 
-const client = generateClient<Schema>();
+//const client = generateClient<Schema>();
 
 function App() {
-
+  const { signOut } = useAuthenticator();
 
   return (
     <main>
       <h1>Storage Test</h1>
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
