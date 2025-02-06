f4b846e2e4e0aec60cf8f3852742b7e67cbf682c
Xin Qiao <qiaoxin136@gmail.com>
2025-02-06 11:49:33 -0500

    second

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,7 +1,7 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'amplifyTeamDrive',
+  name: 'react-storage',
   access: (allow) => ({
     'profile-pictures/{entity_id}/*': [
       allow.guest.to(['read']),
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
-//import { useEffect, useState } from "react";
+import  { useState } from "react";
 import { useAuthenticator } from '@aws-amplify/ui-react';
 //import type { Schema } from "../amplify/data/resource";
 //import { generateClient } from "aws-amplify/data";
+import { uploadData } from 'aws-amplify/storage';
 
 //const client = generateClient<Schema>();
 
 function App() {
   const { signOut } = useAuthenticator();
+  const [file, setFile] = useState();
+
+  const handleChange = (event: any) => {
+    setFile(event.target.files?.[0]);
+  };
+
+  const handleClick = () => {
+    if (!file) {
+      return;
+    }
+    uploadData({
+      path: `picture-submissions/`,
+      data: file,
+    });
+  };
 
   return (
     <main>
       <h1>Storage Test</h1>
       <button onClick={signOut}>Sign out</button>
+      <input type="file" onChange={handleChange} />
+      <button onClick={handleClick}>Upload</button>
     </main>
   );
 }
